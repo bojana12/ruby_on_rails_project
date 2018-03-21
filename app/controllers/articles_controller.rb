7894673905aa8253245c7fb352d4167eeab3a304
Bojana <bojana.karakacev@gmail.com>
2018-03-21 23:35:45 +0100

    Add blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,9 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
@@ -17,7 +20,7 @@ class ArticlesController < ApplicationController
     @article.save
 
     flash.notice = "Article '#{@article.title}' was created!"
-    
+
     redirect_to article_path(@article)
   end
 
